12c8307cad6dea10775a1064d5133d4ebe0e2a85
MostafaFathy367 <mostafa.fathy367@gmail.com>
2021-04-22 04:44:21 +0200

    handel add to cart

--- jumia-front/src/reducers/productDetails.js
+++ jumia-front/src/reducers/productDetails.js
@@ -1,15 +1,15 @@
 var count = 0;
-var cart = JSON.parse(localStorage.getItem("cart"));
 export const productReducer = (state = count, action) => {
+  var cart = JSON.parse(localStorage.getItem("cart"));
   if (action.type === "PLUS") {
     const newCart = [...cart];
     newCart.find((product)=>{
       if(product.nameEn === action.name){
-        count = product.quantity;
-        count++;
-        if(count >= action.quantity){
-          return count = action.quantity;
+        product.selectedQuantity++
+        if(product.selectedQuantity >= product.prodQuantity){
+          product.selectedQuantity = product.prodQuantity;
         }
+        count = product.selectedQuantity;
       }
     })
       localStorage.setItem("cart",JSON.stringify(newCart))
@@ -18,34 +18,37 @@ export const productReducer = (state = count, action) => {
     const newCart = [...cart];
     newCart.find((product,index,arr)=>{
       if(product.nameEn === action.name){
-        count = product.quantity;
-        count--;
-        if(count <= 0){
+        product.selectedQuantity--
+        if(product.selectedQuantity <= 0){
           arr.splice(index,1);
-          return count = 0;
+          product.selectedQuantity = 0;
         }
+        count = product.selectedQuantity;
       }
     })
     localStorage.setItem("cart",JSON.stringify(newCart))
     return (state = count);
   } else if (action.type === "ADDTOCART") {
     count = 1;
-    const product = { ...action.product, quantity: count };
+    const product = { ...action.product, selectedQuantity: count };
     const cartList = [product];
     if (!cart) {
       localStorage.setItem("cart", JSON.stringify(cartList));
     } else {
       const newCart = [...cart];
-      newCart.find((pro,index,arr)=>{
-        if(pro.nameEn===product.nameEn){
-          localStorage.setItem("cart",JSON.stringify(newCart));
-        } else {
-          newCart.push(product);
-          localStorage.setItem("cart",JSON.stringify(newCart));
+      newCart.push(product);
+      localStorage.setItem("cart",JSON.stringify(newCart));
+    }
+
+    return (state = count);
+  } else if (action.type==="LOAD"){
+    if(cart){
+      cart.find(product=>{
+        if(product.nameEn===action.name){
+          count = product.selectedQuantity;
         }
       })
     }
-
     return (state = count);
   }
   return state;
