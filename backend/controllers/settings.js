349e9305f503370c3ae7ceb8237bac3acbd7acb5
mohamed <mo7hamedma7hmod@gmail.com>
2021-04-27 21:08:27 +0200

    add sliders end point

--- backend/controllers/settings.js
+++ backend/controllers/settings.js
@@ -1,7 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const Settings = require("../models/settings.js");
+const { authenticateToken, checkIsAdmin } = require("../models/jwt");
 
+//add sliders
+router.post("/add-Sliders", authenticateToken, checkIsAdmin, (req, res) => {
+  let body = req.body;
+  let sliders = new Settings(body);
+  sliders
+    .save()
+    .then((sliders) => {
+      res.send({
+        sliders,
+        notice: "successfully created the Settings",
+      });
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
 
 // ui end user
 router.get("/get-sliders", (req, res) => {
@@ -14,7 +31,6 @@ router.get("/get-sliders", (req, res) => {
     });
 });
 
-
 // dashboard
 router.get("/get-settings", (req, res) => {
   Settings.find({})
@@ -25,7 +41,5 @@ router.get("/get-settings", (req, res) => {
       res.status(404).send(err);
     });
 });
-
-
 
 module.exports = router;
